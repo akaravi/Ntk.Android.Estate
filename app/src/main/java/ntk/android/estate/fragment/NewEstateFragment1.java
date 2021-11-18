25a5e8cecc971bae5f5120850a00c8ac5ae86d32
work-Idev <parishani@IMT-ISF.local>
2021-11-18 12:01:05 +0330

    init model

--- app/src/main/java/ntk/android/estate/fragment/NewEstateFragment1.java
+++ app/src/main/java/ntk/android/estate/fragment/NewEstateFragment1.java
@@ -8,16 +8,19 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.google.android.material.textfield.MaterialAutoCompleteTextView;
+import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import es.dmoral.toasty.Toasty;
 import ntk.android.base.adapter.SpinnerAdapter;
 import ntk.android.base.config.NtkObserver;
 import ntk.android.base.config.ServiceExecute;
 import ntk.android.base.entitymodel.base.ErrorException;
 import ntk.android.base.entitymodel.base.FilterModel;
 import ntk.android.base.entitymodel.core.CoreLocationModel;
+import ntk.android.base.entitymodel.estate.EstatePropertyModel;
 import ntk.android.base.fragment.BaseFragment;
 import ntk.android.base.services.core.CoreLocationService;
 import ntk.android.estate.R;
@@ -34,6 +37,19 @@ public class NewEstateFragment1 extends BaseFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         estateActivity().showProgress();
+        TextInputEditText codeEt = findViewById(R.id.EstateCodeEditText);
+        TextInputEditText titleEt = findViewById(R.id.EstateTitleEditText);
+        TextInputEditText descEt = findViewById(R.id.EstateDescEditText);
+        TextInputEditText addressEt = findViewById(R.id.EstateAddressEditText);
+        EstatePropertyModel model = estateActivity().model();
+        if (model.CaseCode != null)
+            codeEt.setText(model.CaseCode);
+        if (model.Title != null)
+            titleEt.setText(model.Title);
+        if (model.Description != null)
+            descEt.setText(model.Description);
+        if (model.Address != null)
+            addressEt.setText(model.Address);
         getData();
     }
 
@@ -49,7 +65,7 @@ public class NewEstateFragment1 extends BaseFragment {
                 SpinnerAdapter<CoreLocationModel> locationAdapter = new SpinnerAdapter<CoreLocationModel>(getContext(), names);
                 spinner.setOnItemClickListener((parent, view, position, id) -> {
                     CoreLocationModel selectedModel = model.ListItems.get(position);
-                    //todo set id
+                    estateActivity().model().LinkLocationId = selectedModel.Id;
                 });
                 spinner.setAdapter(locationAdapter);
                 // Do something for lollipop and above versions
@@ -72,6 +88,31 @@ public class NewEstateFragment1 extends BaseFragment {
     }
 
     public boolean isValidForm() {
+        TextInputEditText codeEt = findViewById(R.id.EstateCodeEditText);
+        TextInputEditText titleEt = findViewById(R.id.EstateTitleEditText);
+        TextInputEditText descEt = findViewById(R.id.EstateDescEditText);
+        TextInputEditText addressEt = findViewById(R.id.EstateAddressEditText);
+        if (codeEt.getText().toString().trim().equals("")) {
+            Toasty.error(getContext(), "کد ملک را وارد نمایید", Toasty.LENGTH_LONG, true).show();
+            return false;
+        } else
+            estateActivity().model().CaseCode = codeEt.getText().toString().trim();
+        if (titleEt.getText().toString().trim().equals("")) {
+            Toasty.error(getContext(), "عنوان  را وارد نمایید", Toasty.LENGTH_LONG, true).show();
+            return false;
+        } else
+            estateActivity().model().Title = titleEt.getText().toString().trim();
+        if (descEt.getText().toString().trim().equals("")) {
+            Toasty.error(getContext(), "توضیحات  را وارد نمایید", Toasty.LENGTH_LONG, true).show();
+            return false;
+        } else
+            estateActivity().model().Description = descEt.getText().toString().trim();
+        if (addressEt.getText().toString().trim().equals("")) {
+            Toasty.error(getContext(), " آدرس را وارد نمایید", Toasty.LENGTH_LONG, true).show();
+            return false;
+        } else
+            estateActivity().model().Address = addressEt.getText().toString().trim();
+
         return true;
     }
 }
