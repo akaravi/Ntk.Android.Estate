d502967811018158f97ecd32e512de974309f6ef
ixiflutter1313 <mj.parishani@outlook.com>
2021-11-22 21:10:14 +0330

    Update PropertyDetailGroupsAdapter.java

--- app/src/main/java/ntk/android/estate/adapter/PropertyDetailGroupsAdapter.java
+++ app/src/main/java/ntk/android/estate/adapter/PropertyDetailGroupsAdapter.java
@@ -49,22 +49,27 @@ public class PropertyDetailGroupsAdapter extends BaseRecyclerAdapter<EstatePrope
         holder.title.setText(item.Title);
         holder.rc.setAdapter(new PropertyDetailValueAdapter(item.PropertyValues));
         holder.rc.setLayoutManager(new GridLayoutManager(holder.itemView.getContext(), 2));
-        if (position==list.size()-1)
+        if (position == list.size() - 1)
             holder.line.setVisibility(View.GONE);
         else
             holder.line.setVisibility(View.VISIBLE);
+        if (item.PropertyValues.size() == 0)
+            holder.itemView.setVisibility(View.GONE);
+        else
+            holder.itemView.setVisibility(View.VISIBLE);
     }
 
     public class VH extends RecyclerView.ViewHolder {
         TextView title;
         RecyclerView rc;
         View line;
+
         public VH(@NonNull View itemView) {
             super(itemView);
             title = itemView.findViewById(R.id.propertyGroupTitle);
             title.setTypeface(FontManager.T1_Typeface(itemView.getContext()));
             rc = itemView.findViewById(R.id.detailsValues);
-            line=itemView.findViewById(R.id.lineImage);
+            line = itemView.findViewById(R.id.lineImage);
         }
 
     }
