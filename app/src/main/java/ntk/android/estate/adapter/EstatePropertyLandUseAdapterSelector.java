eb4e46b6f1e7b29434422451e397dcd38701d41f
ixiflutter1313 <mj.parishani@outlook.com>
2021-11-28 21:24:45 +0330

    Update EstatePropertyLandUseAdapterSelector.java

--- app/src/main/java/ntk/android/estate/adapter/EstatePropertyLandUseAdapterSelector.java
+++ app/src/main/java/ntk/android/estate/adapter/EstatePropertyLandUseAdapterSelector.java
@@ -1,5 +1,6 @@
 package ntk.android.estate.adapter;
 
+import android.graphics.Typeface;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -13,6 +14,7 @@ import java.util.List;
 import java9.util.function.Consumer;
 import ntk.android.base.adapter.BaseRecyclerAdapter;
 import ntk.android.base.entitymodel.estate.EstatePropertyTypeLanduseModel;
+import ntk.android.base.utill.FontManager;
 import ntk.android.estate.R;
 
 public class EstatePropertyLandUseAdapterSelector extends BaseRecyclerAdapter<EstatePropertyTypeLanduseModel, EstatePropertyLandUseAdapterSelector.VH> {
@@ -58,6 +60,8 @@ public class EstatePropertyLandUseAdapterSelector extends BaseRecyclerAdapter<Es
         public VH(@NonNull View itemView) {
             super(itemView);
             title = itemView.findViewById(R.id.chip);
+            Typeface typeface = FontManager.T1_Typeface(itemView.getContext());
+            title.setTypeface(typeface);
         }
     }
 }
