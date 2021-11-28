b0da064eedf14ec8f1a40789c92dfee5b2d9695c
ixiflutter1313 <mj.parishani@outlook.com>
2021-11-28 22:24:37 +0330

    fix padding between details

--- app/src/main/java/ntk/android/estate/adapter/EstatePropertyDetailGroupAdapterSelector.java
+++ app/src/main/java/ntk/android/estate/adapter/EstatePropertyDetailGroupAdapterSelector.java
@@ -1,6 +1,7 @@
 package ntk.android.estate.adapter;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -13,6 +14,7 @@ import java.util.List;
 
 import ntk.android.base.adapter.BaseRecyclerAdapter;
 import ntk.android.base.entitymodel.estate.EstatePropertyDetailGroupModel;
+import ntk.android.base.utill.FontManager;
 import ntk.android.estate.R;
 
 public class EstatePropertyDetailGroupAdapterSelector extends BaseRecyclerAdapter<EstatePropertyDetailGroupModel, EstatePropertyDetailGroupAdapterSelector.VH> {
@@ -32,18 +34,29 @@ public class EstatePropertyDetailGroupAdapterSelector extends BaseRecyclerAdapte
         holder.title.setText(item.Title);
         holder.rc.setLayoutManager(new GridLayoutManager(holder.getContext(), 2));
         holder.rc.setAdapter(new EstatePropertyDetailAdapterSelector(item));
-
+        if (position==list.size()-1){
+            holder.itemSeparator.setVisibility(View.GONE);
+            holder.lastItemPadding.setVisibility(View.VISIBLE);
+        }else{
+            holder.itemSeparator.setVisibility(View.VISIBLE);
+            holder.lastItemPadding.setVisibility(View.GONE);
+        }
     }
 
     public class VH extends RecyclerView.ViewHolder {
 
         TextView title;
         RecyclerView rc;
-
+        View itemSeparator;
+        View lastItemPadding;
         public VH(@NonNull View itemView) {
             super(itemView);
             title = itemView.findViewById(R.id.titleTv);
             rc = itemView.findViewById(R.id.estateDetailGroupValueRc);
+            itemSeparator=itemView.findViewById(R.id.itemSeparator);
+            lastItemPadding=itemView.findViewById(R.id.lastItemPadding);
+            Typeface typeface = FontManager.T1_Typeface(itemView.getContext());
+            title.setTypeface(typeface);
         }
 
         public Context getContext() {
