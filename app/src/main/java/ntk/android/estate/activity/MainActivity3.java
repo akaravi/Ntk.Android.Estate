78e0db5aa3625a57176ca1031294012b3f35e7aa
ixiflutter1313 <mj.parishani@outlook.com>
2022-01-02 21:19:08 +0330

    fix bug

--- app/src/main/java/ntk/android/estate/activity/MainActivity3.java
+++ app/src/main/java/ntk/android/estate/activity/MainActivity3.java
@@ -3,7 +3,9 @@ package ntk.android.estate.activity;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -81,6 +83,13 @@ public class MainActivity3 extends BaseMainActivity {
         findViewById(R.id.shareQrCode).setOnClickListener(v -> onInviteMethod());
         //click search
         findViewById(R.id.searchBtn).setOnClickListener(view -> Search());
+        ((EditText) findViewById(R.id.searchEt)).setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                Search();
+                return true;
+            }
+            return false;
+        });
         TextView rowTitle1 = findViewById(R.id.row1).findViewById(R.id.title);
         TextView rowSeeMore1 = findViewById(R.id.row1).findViewById(R.id.seeMore);
         TextView rowTitle2 = findViewById(R.id.row2).findViewById(R.id.title);
