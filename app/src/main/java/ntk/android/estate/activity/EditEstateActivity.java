af094e766c847c610ee323ddf66ea6f205de1343
ixiflutter1313 <mj.parishani@outlook.com>
2022-07-06 23:27:03 +0430

    Update EditEstateActivity.java

--- app/src/main/java/ntk/android/estate/activity/EditEstateActivity.java
+++ app/src/main/java/ntk/android/estate/activity/EditEstateActivity.java
@@ -8,12 +8,15 @@ import androidx.annotation.Nullable;
 import java.util.ArrayList;
 
 import es.dmoral.toasty.Toasty;
+import java9.util.stream.StreamSupport;
 import ntk.android.base.Extras;
 import ntk.android.base.config.ErrorExceptionObserver;
 import ntk.android.base.config.GenericErrors;
 import ntk.android.base.config.NtkObserver;
 import ntk.android.base.config.ServiceExecute;
 import ntk.android.base.entitymodel.base.ErrorException;
+import ntk.android.base.entitymodel.estate.EstatePropertyDetailGroupModel;
+import ntk.android.base.entitymodel.estate.EstatePropertyDetailValueModel;
 import ntk.android.base.entitymodel.estate.EstatePropertyModel;
 import ntk.android.base.services.estate.EstatePropertyService;
 import ntk.android.base.utill.AppUtil;
@@ -39,8 +42,29 @@ public class EditEstateActivity extends NewEstateActivity {
                         @Override
                         protected void SuccessResponse(ErrorException<EstatePropertyModel> ContentResponse) {
                             model = ContentResponse.Item;
+                            //create list of values base on details
+//                            StreamSupport.stream(model.PropertyDetailGroups).
+//                                    forEach(estatePropertyDetailGroupModel -> {
+//                                        estatePropertyDetailGroupModel.PropertyValues = new ArrayList<>();
+//                                        StreamSupport.stream(estatePropertyDetailGroupModel.PropertyDetails)
+//                                                .forEach(estatePropertyDetailModel -> {
+//                                                    EstatePropertyDetailValueModel value = new EstatePropertyDetailValueModel();
+//                                                    value.LinkPropertyDetailId = estatePropertyDetailModel.Id;
+//                                                    value.PropertyDetail = estatePropertyDetailModel;
+//                                                    estatePropertyDetailGroupModel.PropertyValues.add(value);
+//                                                });
+//                                    });
                             //sync property and its values
-                            PropertyDetailGroupsAdapter.INIT(model.PropertyDetailGroups, model.PropertyDetailValues);
+                            for (EstatePropertyDetailGroupModel detail :
+                                    model.PropertyDetailGroups) {
+                                detail.PropertyValues = new ArrayList<>();
+                                for (EstatePropertyDetailValueModel value :
+                                        model.PropertyDetailValues) {
+                                    if ( StreamSupport.stream(detail.PropertyDetails).anyMatch(j -> j.Id.equals(value.LinkPropertyDetailId))) {
+                                        detail.PropertyValues.add(value);
+                                    }
+                                }
+                            }
                             switcher.showContentView();
                             showFragment1();
 
