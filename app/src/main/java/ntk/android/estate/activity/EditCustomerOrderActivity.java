3a72873e22def3833a196f2a0fa627eafc423865
MJ.Dell <javadparishani@gmail.com>
2022-12-04 15:20:16 +0330

    edit order

--- app/src/main/java/ntk/android/estate/activity/EditCustomerOrderActivity.java
+++ app/src/main/java/ntk/android/estate/activity/EditCustomerOrderActivity.java
@@ -7,12 +7,18 @@ import androidx.annotation.NonNull;
 
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
+
 import es.dmoral.toasty.Toasty;
+import java9.util.stream.StreamSupport;
 import ntk.android.base.Extras;
+import ntk.android.base.config.ErrorExceptionObserver;
 import ntk.android.base.config.NtkObserver;
 import ntk.android.base.config.ServiceExecute;
 import ntk.android.base.entitymodel.base.ErrorException;
 import ntk.android.base.entitymodel.estate.EstateCustomerOrderModel;
+import ntk.android.base.entitymodel.estate.EstatePropertyDetailGroupModel;
+import ntk.android.base.entitymodel.estate.EstatePropertyDetailValueModel;
 import ntk.android.base.services.estate.EstateCustomerOrderService;
 
 public class EditCustomerOrderActivity extends NewCustomerOrderActivity {
@@ -25,7 +31,39 @@ public class EditCustomerOrderActivity extends NewCustomerOrderActivity {
     @Override
     protected void afterCreate() {
         model = new Gson().fromJson(getIntent().getExtras().getString(Extras.Extra_5_ARG), EstateCustomerOrderModel.class);
-        showFragment1();
+       getone();
+    }
+
+    private void getone() {
+        ServiceExecute.execute(new EstateCustomerOrderService(this).getOneByEdit(model.Id))
+                .subscribe(new ErrorExceptionObserver<EstateCustomerOrderModel>(switcher::showErrorView) {
+
+                    @Override
+                    protected void SuccessResponse(ErrorException<EstateCustomerOrderModel> ContentResponse) {
+                        model = ContentResponse.Item;
+                        //sync property and its values
+                        for (EstatePropertyDetailGroupModel detail :
+                                model.PropertyDetailGroups) {
+                            detail.PropertyValues = new ArrayList<>();
+                            for (EstatePropertyDetailValueModel value :
+                                    model.PropertyDetailValues) {
+                                if ( StreamSupport.stream(detail.PropertyDetails).anyMatch(j -> j.Id.equals(value.LinkPropertyDetailId))) {
+                                    detail.PropertyValues.add(value);
+                                }
+                            }
+                        }
+                        switcher.showContentView();
+                        showFragment1();
+
+                    }
+
+                    @Override
+                    protected Runnable tryAgainMethod() {
+                        return EditCustomerOrderActivity.this::getone;
+                    }
+
+                });
+
     }
 
     @Override
